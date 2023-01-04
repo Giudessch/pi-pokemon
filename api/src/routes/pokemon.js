cc1f789816a97150135978d13f9ff971c1098e31
Giuliana Schiebelbein <giulianaschiebelbein2@gmail.com>
2023-01-03 21:40:56 -0300

    ✨ Agrego rutas en el backend

--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const utils = require('../utils/utils');
+const { Pokemons } = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -14,26 +15,45 @@ const utils = require('../utils/utils');
  * 
  */
 router.get('/', async (req, res) => {
-    const query = req.query;
-    const pageIndex = req.query.page_index ? Number(req.query.page_index) : 1;
-    const pageSize = req.query.page_size ? Number(req.query.page_size) : 12;
+  const query = req.query;
+  const pageIndex = req.query.page_index ? Number(req.query.page_index) : 1;
+  const pageSize = req.query.page_size ? Number(req.query.page_size) : 12;
 
-    const pokemons = await utils.getPokemonsApi({ limit: pageSize, offset: (pageIndex - 1) * pageSize });
+  const pokemons = await utils.getPokemonsApi({ limit: pageSize, offset: (pageIndex - 1) * pageSize });
 
-    return res.send(pokemons);
+  return res.send(pokemons);
 });
 
 /**
  * GET /pokemons/{idPokemon}
  */
+router.get('/:idPokemon', async (req, res) => {
+  const idPokemon = req.params.idPokemon;
 
-/**
- * GET /pokemons?name="..."
- */
+  const pokemon = await Pokemons.find({ where: { id: idPokemon } });
+
+  return res.send(pokemon);
+});
 
 /**
  * POST /pokemons
+ * body: {
+ *     name: string
+ *     lifetime: number
+ *     attack: number
+ *     speed: number
+ *     height: number
+ *     weight: number
+ *     types: number[]
+ * }
  */
+router.post('/', async (req, res) => {
+  const body = req.body;
+
+  const pokemon = await Pokemons.create(body);
+
+  return res.send(pokemon);
+});
 
 
 module.exports = router;
